8edfc00716ff38e04086343ec4a6aa60023118dc
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-03-31 11:41:16 -0700

    User Login

--- DevConnector/client/src/components/auth/Login.js
+++ DevConnector/client/src/components/auth/Login.js
@@ -1,7 +1,10 @@
 import React, { Fragment, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { login } from '../../actions/auth';
 
-const Login = () => {
+const Login = ({ login, isAuthenticated }) => {
   const [formData, setFormData]= useState({
     email: '',
     password: ''
@@ -13,10 +16,13 @@ const Login = () => {
 
   const onSubmit = async e => {
     e.preventDefault();
-
-    console.log('SUCCESS');
+    login(email, password);
   };
-  
+  //Redirect if logged in
+  if(isAuthenticated) {
+    return <Navigate to="/dashboard" />;
+  }
+
   return (
 	<Fragment> 
     <h1 className="large text-primary">Sign In</h1>
@@ -51,5 +57,13 @@ const Login = () => {
   );
 };
 
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
+};
 
-export default Login;
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { login })(Login);
