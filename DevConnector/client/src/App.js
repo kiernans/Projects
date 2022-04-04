3d09657cf61ab717453cd2cc3ec5b7a9a9268f2f
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-04-04 12:07:09 -0700

    CreateProfile component

--- DevConnector/client/src/App.js
+++ DevConnector/client/src/App.js
@@ -7,6 +7,7 @@ import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Landing'
 import Register from './components/auth/Register'
 import Dashboard from './components/dashboard/Dashboard'
+import CreateProfile from './components/profile-forms/CreateProfile'
 import PrivateRoute from './components/routing/PrivateRoute'
 import Login from './components/auth/Login'
 import Alert from './components/layout/Alert'
@@ -37,6 +38,7 @@ const App = () => {
           <Route path="register" element={ <Register />} />
           <Route path="login" element={ <Login />} />
           <Route path="dashboard" element={ <PrivateRoute component={Dashboard} />} />
+          <Route path="create-profile" element={ <PrivateRoute component={CreateProfile} />} />
         </Routes>
       </Router>
     </Provider>
