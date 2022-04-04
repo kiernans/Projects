1214456466a038ff36a3e849f6e467a885fd6e22
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-04-04 14:36:28 -0700

    Add Education and experiences

--- DevConnector/client/src/App.js
+++ DevConnector/client/src/App.js
@@ -9,6 +9,8 @@ import Register from './components/auth/Register'
 import Dashboard from './components/dashboard/Dashboard'
 import CreateProfile from './components/profile-forms/CreateProfile'
 import EditProfile from './components/profile-forms/EditProfile';
+import AddExperience from './components/profile-forms/AddExperience';
+import AddEducation from './components/profile-forms/AddEducation';
 import PrivateRoute from './components/routing/PrivateRoute'
 import Login from './components/auth/Login'
 import Alert from './components/layout/Alert'
@@ -41,6 +43,8 @@ const App = () => {
           <Route path="dashboard" element={ <PrivateRoute component={Dashboard} />} />
           <Route path="create-profile" element={ <PrivateRoute component={CreateProfile} />} />
           <Route path="edit-profile" element={ <PrivateRoute component={EditProfile} />} />
+          <Route path="add-experience" element={ <PrivateRoute component={AddExperience} />} />
+          <Route path="add-education" element={ <PrivateRoute component={AddEducation} />} />
         </Routes>
       </Router>
     </Provider>
