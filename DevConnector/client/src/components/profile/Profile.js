ec44be53b5723cc1dc8db69393724c9adc082dbc
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-04-05 13:58:26 -0700

    ProfileTop and ProfileAbout

--- DevConnector/client/src/components/profile/Profile.js
+++ DevConnector/client/src/components/profile/Profile.js
@@ -3,13 +3,15 @@ import { Link, useParams } from 'react-router-dom';
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
+import ProfileTop from './ProfileTop';
+import ProfileAbout from './ProfileAbout';
 import { getProfileById } from '../../actions/profile';
 
 const Profile = ({ getProfileById, profile: { profile, loading }, auth }) => {
 	const { id } = useParams();
 	useEffect(() => {
 		getProfileById(id);
-	}, [getProfileById]);
+	}, [getProfileById, id]);
   
 	return (
 	<Fragment>
@@ -17,11 +19,18 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth }) => {
 				<Link to='/profiles' className='btn btn-light'>
 					Back to profiles
 				</Link>
-				{auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && (
+				{auth.isAuthenticated && 
+				auth.loading === false && 
+				auth.user._id === profile.user._id && 
+				(
 					<Link to='/edit-profile' className="btn btn-dark">
 						Edit Profile
 					</Link>
 				)}
+				 <div class="profile-grid my-1">
+					 <ProfileTop profile={profile} />
+					 <ProfileAbout profile={profile} />
+				 </div>
 			</Fragment>}
 	</Fragment>
   )
