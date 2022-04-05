df963c1e36891a5bd46bab4a9ebd43acb89e293b
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-04-05 14:53:33 -0700

    Displaying Github repos

--- DevConnector/routes/api/profile.js
+++ DevConnector/routes/api/profile.js
@@ -328,10 +328,15 @@ router.delete('/education/:edu_id', auth, async (req,res) => {
 router.get('/github/:username', (req, res) => {
 	try {
 		const options = {
-			uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientID')}&client_secret=${config.get('githubSecret')}`,
+			uri: encodeURI(
+			  `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+			),
 			method: 'GET',
-			headers: { 'user-agent': 'node.js' }
-		};
+			headers: {
+			  'user-agent': 'node.js',
+			  Authorization: `token ${config.get('githubToken')}`
+			}
+		  };
 
 		request(options, (error, response, body) => {
 			if(error) console.error(error);
