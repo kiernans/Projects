d23d7030a63bad75109ff86c7ba4dd64f79664bf
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-04-06 13:09:15 -0700

    Adding Posts

--- DevConnector/client/src/components/posts/Post.js
+++ DevConnector/client/src/components/posts/Post.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import PostItem from './PostItem';
+import PostForm from './PostForm';
 import { getPosts } from '../../actions/post'
 
 const Post = ({ getPosts, post: { posts, loading } }) => {
@@ -10,21 +11,23 @@ useEffect(() => {
 	getPosts();
 }, [getPosts]);
 
-  return (
-	loading ? <Spinner /> : <Fragment> 
+  return loading ? (
+	<Spinner />
+	) : (
+	<Fragment> 
 		<h1 className="large text-primary">Posts</h1>
 		<p className="lead">
 			<i className="fas fa-user"></i> Welcome to the community
 		</p>
-		{/* PostForm */}
+		<PostForm />
 		<div className="posts">
 			{posts.map(post => (
 				<PostItem key={post._id} post={post} />
 			))}
 		</div>
 	</Fragment>
-  )
-}
+  );
+};
 
 Post.propTypes = {
 	getPosts: PropTypes.func.isRequired,
